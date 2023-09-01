5603eb489afbb6bb9c9cd84e0a95a4f12ee3e6aa
Yunus Yasar <yunusyasar1903@gmail.com>
2023-09-01 19:07:36 +0200

    completed

--- js/add_task.js
+++ js/add_task.js
@@ -7,7 +7,7 @@ let categories = [
   {name: 'Media', color: '#00d345'},
   {name: 'Marketing', color: '#bb051d'},
 ];
-let prios = ['urgent', 'medium', 'low'];
+
 let currentPriority = null;
 let selectedPrio = null;
 let currentSubtasks = [];
@@ -15,8 +15,9 @@ let currentSubtasks = [];
 async function init() {
   await includeHTML();
   getCategory();
-  loadContacts();
-  loadTasks();
+  await loadTasks();
+  await loadContacts();
+  renderToDoCard();
 }
 
 async function loadTasks() {
@@ -40,41 +41,43 @@ async function loadContacts() {
   } catch (error) {
     console.error('Could not load contacts:', error);
   }
-  renderAssignedContacts();
+  renderAssignedContacts('dropdownContentContacts');
 }
-function renderAssignedContacts() {
-  let contactsList = document.getElementById('dropdownContentContacts');
+
+function renderAssignedContacts(targetDivId, task = null) {
+  let contactsList = document.getElementById(targetDivId);
+  if (!contactsList) {
+    console.error(`Element with ID ${targetDivId} not found.`);
+    return;
+  }
   contactsList.innerHTML = '';
   let sortedContacts = sortContactsByName();
 
   for (let index = 0; index < sortedContacts.length; index++) {
     let contact = sortedContacts[index];
     contactsList.style.display = 'none';
-    appendAssignedContact(contactsList, contact, index);
+    appendAssignedContact(contactsList, contact, index, task);
   }
 }
-function appendAssignedContact(contactsList, contact, index) {
-  let color = contact.color;
+function appendAssignedContact(contactsList, contact, index, task = null) {
   let initials = getInitials(contact.name);
+  let isChecked = '';
+  // Prüfe, ob der Kontakt bereits zugewiesen ist, wenn ein Task übergeben wurde
+  if (task) {
+    for (let assignedContact of task.contacts) {
+      if (assignedContact.name === contact.name && assignedContact.email === contact.email) {
+        isChecked = 'checked';
+        break;
+      }
+    }
+  }
   let contactContainer = document.createElement('div');
 
-  contactContainer.innerHTML = `
-      <div class="contact-card-assigned">
-          <div class="contact-avatar-assigned" style="background-color: ${color}">
-              <span>${initials}</span>
-          </div>
-          <div class="assigned-contact-choice">
-              <label class="contact-name">${contact.name}
-              <input type="checkbox" class="assigned-checkbox" value="${index}">
-              </label>
-          </div>
-      </div>
-  `;
+  contactContainer.innerHTML = appendAssignedContactHTML(contact, initials, index, isChecked);
 
   contactContainer.onclick = function (event) {
     event.stopPropagation();
   };
-
   contactsList.appendChild(contactContainer);
 }
 
@@ -88,58 +91,6 @@ function getSelectedContacts() {
   return selectedContacts;
 }
 
-// function getCategory() {
-//   let dropdownContent = document.getElementById('dropdownContent');
-
-//   for (let i = 0; i < categories.length; i++) {
-//     let category = categories[i];
-//     let categoryDiv = document.createElement('div');
-//     dropdownContent.style.display = 'none';
-//     let circle = document.createElement('span');
-//     circle.style.backgroundColor = category.color;
-//     circle.className = 'circle';
-
-//     categoryDiv.appendChild(document.createTextNode(category.name));
-//     categoryDiv.appendChild(circle);
-//     categoryDiv.onclick = function (event) {
-//       // Verhindert, dass das Klick-Event nach oben weitergegeben wird
-//       event.stopPropagation();
-//       selectCategory(category.name, category.color);
-//     };
-
-//     dropdownContent.appendChild(categoryDiv);
-//   }
-// }
-function getCategory() {
-  let dropdownContent = document.getElementById('dropdownContent');
-
-  if (!dropdownContent) {
-    console.error("Element 'dropdownContent' existiert nicht im DOM.");
-    return; // Verlässt die Funktion, wenn das Element nicht existiert
-  }
-
-  for (let i = 0; i < categories.length; i++) {
-    let category = categories[i];
-    let categoryDiv = document.createElement('div');
-
-    dropdownContent.style.display = 'none';
-
-    let circle = document.createElement('span');
-    circle.style.backgroundColor = category.color;
-    circle.className = 'circle';
-
-    categoryDiv.appendChild(document.createTextNode(category.name));
-    categoryDiv.appendChild(circle);
-    categoryDiv.onclick = function (event) {
-      // Verhindert, dass das Klick-Event nach oben weitergegeben wird
-      event.stopPropagation();
-      selectCategory(category.name, category.color);
-    };
-
-    dropdownContent.appendChild(categoryDiv);
-  }
-}
-
 function toggleDropdownContacts() {
   let contactsList = document.getElementById('dropdownContentContacts');
   if (contactsList.style.display === 'none' || contactsList.style.display === '') {
@@ -148,13 +99,42 @@ function toggleDropdownContacts() {
     contactsList.style.display = 'none';
   }
 }
-function toggleDropdown() {
+
+// Hauptfunktion zum Abrufen von Kategorien und Hinzufügen zum Dropdown
+function getCategory() {
   let dropdownContent = document.getElementById('dropdownContent');
-  if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
-    dropdownContent.style.display = 'block';
-  } else {
-    dropdownContent.style.display = 'none';
+
+  if (!dropdownContent) {
+    console.error("Element 'dropdownContent' existiert nicht im DOM.");
+    return; // Verlässt die Funktion, wenn das Element nicht existiert
   }
+
+  dropdownContent.style.display = 'none';
+
+  for (let i = 0; i < categories.length; i++) {
+    let category = categories[i];
+    let categoryDiv = createCategoryDiv(category);
+    dropdownContent.appendChild(categoryDiv);
+  }
+}
+// Funktion zum Erstellen eines einzelnen Kategorie-Divs
+function createCategoryDiv(category) {
+  let categoryDiv = document.createElement('div');
+
+  let circle = document.createElement('span');
+  circle.style.backgroundColor = category.color;
+  circle.className = 'circle';
+
+  categoryDiv.appendChild(document.createTextNode(category.name));
+  categoryDiv.appendChild(circle);
+
+  categoryDiv.onclick = function (event) {
+    // Verhindert, dass das Klick-Event nach oben weitergegeben wird
+    event.stopPropagation();
+    selectCategory(category.name, category.color);
+  };
+
+  return categoryDiv;
 }
 
 function selectCategory(name, color) {
@@ -172,9 +152,17 @@ function selectCategory(name, color) {
   document.querySelector('.dropdown').appendChild(selectedCircle);
 }
 
+function toggleDropdown() {
+  let dropdownContent = document.getElementById('dropdownContent');
+  if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
+    dropdownContent.style.display = 'block';
+  } else {
+    dropdownContent.style.display = 'none';
+  }
+}
+
 function setPrio(prioType) {
-  // Alle Klassen und d-none von den Bildern entfernen
-  const boxes = ['urgent-prio-box', 'medium-prio-box', 'low-prio-box'];
+  const boxes = ['urgent-prio-box', 'medium-prio-box', 'low-prio-box']; // Alle Klassen und d-none von den Bildern entfernen
   boxes.forEach(boxId => {
     const box = document.getElementById(boxId);
     box.classList.remove('urgent', 'medium', 'low');
@@ -183,16 +171,12 @@ function setPrio(prioType) {
     const coloredIcon = box.querySelector(':nth-child(2)');
     coloredIcon.classList.remove('d-none');
   });
-
-  // Dann die Klasse dem angeklickten Element hinzufügen
-  const currentBox = document.getElementById(`${prioType}-prio-box`);
+  const currentBox = document.getElementById(`${prioType}-prio-box`); // Dann die Klasse dem angeklickten Element hinzufügen
   currentBox.classList.add(prioType);
   const whiteIcon = currentBox.querySelector('.white-icon');
   whiteIcon.classList.remove('d-none');
   const coloredIcon = currentBox.querySelector(':nth-child(2)');
   coloredIcon.classList.add('d-none');
-
-  // Setze den aktuellen Wert von priority
   currentPriority = prioType;
 }
 
@@ -202,8 +186,7 @@ async function addTask() {
   const selectedCategory = document.getElementById('selectedCategory').textContent.trim();
   const selectedContacts = getSelectedContacts();
   const dueDate = document.getElementById('due-date').value;
-
-  // Verwende currentPriority direkt
+  //zorunlu alan olark priority
   const task = {
     title,
     description,
@@ -214,7 +197,6 @@ async function addTask() {
     subtasks: [...currentSubtasks],
     status: 'toDo',
   };
-
   tasks.push(task);
   try {
     await setItem('tasks', JSON.stringify(tasks));
@@ -223,52 +205,69 @@ async function addTask() {
     console.error('Fehler beim Speichern des Tasks:', error);
   }
   clearAll();
+  renderToDoCard('subtask-box');
+  showUserFeedbackMessage('Task successfully created');
+  addTaskModal();
 }
 
 function setMinDate() {
   const today = new Date().toISOString().split('T')[0];
   document.getElementsByName('due-date')[0].setAttribute('min', today);
 }
+
 function clearInput(id) {
   let inputField = document.getElementById(id);
   inputField.value = '';
 }
-function addSubtask() {
-  const input = document.getElementById('subtask-input');
+
+function renderSubtasks(targetDivId, task = null) {
+  console.log('renderSubtasks() wurde aufgerufen, targetDivId:', targetDivId);
+
+  const container = document.getElementById(targetDivId);
+  console.log('Container:', container);
+
+  if (!container) {
+    console.error(`Element mit ID ${targetDivId} nicht gefunden.`);
+    return;
+  }
+  container.innerHTML = '';
+  for (let i = 0; i < currentSubtasks.length; i++) {
+    const subtask = currentSubtasks[i];
+    container.innerHTML += getSubtaskHTML(subtask, i, task, targetDivId);
+  }
+}
+
+function getSubtaskHTML(subtask, i, task = null, targetDivId) {
+  let subtaskName = task ? subtask.name : subtask.name;
+  return /*html*/ `
+    <div class="subtasks-content">
+      <span>
+        - ${subtaskName}
+      </span>
+      <img onclick="deleteSubtask(${i},'${targetDivId}')" class="subtask-img" src="../assets/img/delete.svg" alt="" />
+    </div> 
+    `;
+}
+
+function addSubtask(targetDivId) {
+  console.log('addSubtask() wurde aufgerufen');
+
+  const input = targetDivId == 'subtask-edit-box' ? document.getElementById('subtask-input-edit') : document.getElementById('subtask-input');
   if (input.value.length > 0) {
     const subtask = {
       name: input.value,
       status: 'open',
     };
+    console.log(`Füge Subtask hinzu: ${input.value}`);
     currentSubtasks.push(subtask);
-    renderSubtasks();
+    renderSubtasks(targetDivId);
+    clearInput(input.id);
   }
 }
 
-function renderSubtasks() {
-  const container = document.getElementById('subtask-box');
-  clearInput('subtask-input');
-  container.innerHTML = '';
-
-  for (let i = 0; i < currentSubtasks.length; i++) {
-    const subtask = currentSubtasks[i];
-    container.innerHTML += getSubtaskHTML(subtask, i);
-  }
-}
-
-function getSubtaskHTML(subtask, index) {
-  return /*html*/ `
-  <div class="subtasks-content">
-    <span>
-      - ${subtask.name}
-    </span>
-    <img onclick="deleteSubtask(${index})" class="subtask-img" src="../assets/img/delete.svg" alt="" />
-  </div> 
-  `;
-}
-function deleteSubtask(i) {
+function deleteSubtask(i, targetDivId) {
   currentSubtasks.splice(i, 1);
-  renderSubtasks();
+  renderSubtasks(targetDivId);
 }
 
 function clearAll() {
@@ -278,11 +277,14 @@ function clearAll() {
   document.getElementById('selectedCategory').innerHTML = 'Select task category';
   document.getElementById('due-date').value = '';
   currentSubtasks = [];
-  renderSubtasks();
+  renderSubtasks('subtask-box');
   let existingCircle = document.querySelector('.dropdown .selected-circle');
   if (existingCircle) existingCircle.remove();
-  // Priority-Boxen zurücksetzen
-  const boxes = ['urgent-prio-box', 'medium-prio-box', 'low-prio-box'];
+  resetPrioAndDropdown();
+}
+
+function resetPrioAndDropdown() {
+  const boxes = ['urgent-prio-box', 'medium-prio-box', 'low-prio-box']; // Priority-Boxen zurücksetzen
   boxes.forEach(boxId => {
     const box = document.getElementById(boxId);
     box.classList.remove('urgent', 'medium', 'low');
@@ -291,14 +293,11 @@ function clearAll() {
     const coloredIcon = box.querySelector(':nth-child(2)');
     coloredIcon.classList.remove('d-none');
   });
-  // Alle Checkboxen im Dropdown zurücksetzen
-  const checkboxes = document.querySelectorAll('#dropdownContentContacts input[type="checkbox"]');
+  const checkboxes = document.querySelectorAll('#dropdownContentContacts input[type="checkbox"]'); // Alle Checkboxen im Dropdown zurücksetzen
   checkboxes.forEach(checkbox => {
     checkbox.checked = false;
   });
-
-  // Dropdown mit der ID assignedContactsList schließen
-  const dropdown = document.getElementById('dropdownContentContacts');
+  const dropdown = document.getElementById('dropdownContentContacts'); // Dropdown mit der ID assignedContactsList schließen
   if (dropdown) {
     dropdown.style.display = 'none';
   }
@@ -315,10 +314,11 @@ async function deleteAllTasks() {
 }
 
 function addTaskModal() {
-  // addContactForm.reset();
   let parent = document.getElementById('addTaskModal');
   let child = document.getElementById('addTaskInModal');
 
   parent.classList.toggle('modal-bg-animation');
   child.classList.toggle('modal-animation');
+  clearAll();
+  clearInput('subtask-input');
 }
---
+++ js/board.js
@@ -0,0 +1,349 @@
+let editedPriority = null;
+
+let currentDraggedElement;
+
+function startDragging(index) {
+  currentDraggedElement = index;
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+async function moveTo(status) {
+  tasks[currentDraggedElement]['status'] = status;
+  renderToDoCard();
+  await uploadTasks();
+}
+
+function startDragging(index) {
+  currentDraggedElement = index;
+}
+
+async function uploadTasks() {
+  await setItem('tasks', JSON.stringify(tasks));
+}
+
+async function changeStatusMobile(index, status, doWhat) {
+  const availibleStatus = ['toDo', 'inProgress', 'awaitingFeedback', 'done'];
+  const task = tasks[index];
+  const currentStatusIndex = availibleStatus.indexOf(status);
+
+  if (doWhat === 'previous' && currentStatusIndex > 0) {
+    task.status = availibleStatus[currentStatusIndex - 1];
+  }
+  if (doWhat === 'next' && currentStatusIndex < availibleStatus.length - 1) {
+    task.status = availibleStatus[currentStatusIndex + 1];
+  }
+
+  await uploadTasks();
+  renderToDoCard();
+}
+
+function renderSubtaskProgressBar(task, index) {
+  const container = document.getElementById(`subtask-box-progress-bar-${index}`);
+  const subtasks = task.subtasks;
+
+  if (subtasks && subtasks.length > 0) {
+    const completedSubtasks = getCompletedSubtasks(subtasks);
+    const subtaskPercent = getSubtaskPercent(subtasks, completedSubtasks);
+    container.innerHTML = getProgressBarHTML(subtasks, subtaskPercent, completedSubtasks);
+    container.classList.remove('d-none');
+  } else {
+    container.classList.add('d-none');
+  }
+}
+
+function getCompletedSubtasks(subtasks) {
+  if (subtasks) {
+    let completedSubtasks = 0;
+
+    subtasks.forEach(subtasks => {
+      if (subtasks.status === 'closed') {
+        completedSubtasks++;
+      }
+    });
+
+    return completedSubtasks;
+  }
+}
+
+function getCompletedSubtasks(subtasks) {
+  let completedSubtasks = 0;
+  subtasks.forEach(subtask => {
+    if (subtask.status === 'closed') {
+      completedSubtasks++;
+    }
+  });
+  return completedSubtasks;
+}
+
+function getSubtaskPercent(subtasks, completedSubtasks) {
+  if (subtasks) {
+    const totalSubtasks = subtasks.length;
+    const subtaskPercent = (completedSubtasks / totalSubtasks) * 100;
+
+    return subtaskPercent;
+  }
+}
+
+function getProgressBarHTML(subtasks, subtaskPercent, completedSubtasks) {
+  return /*html*/ `
+    <div class="progress-bar-box">
+    <div style="width: ${subtaskPercent}%;" class="progress-bar-bar"></div>
+    </div>
+    <div class="subtask-text-box">
+    <span>${completedSubtasks}/${subtasks.length}</span> 
+    <span>Done</span>
+    </div>
+  `;
+}
+
+function renderToDoCard() {
+  const statusArr = ['toDo', 'inProgress', 'awaitingFeedback', 'done'];
+
+  statusArr.forEach(status => {
+    const card = document.getElementById(`${status}-area`);
+
+    if (card) {
+      card.innerHTML = '';
+      tasks.forEach((task, index) => {
+        if (task.status === status) {
+          card.innerHTML += generateToDoHTML(task, index);
+          renderSubtaskProgressBar(task, index);
+        }
+      });
+    } else {
+      console.error(`Element mit der ID '${status}-area' wurde nicht gefunden.`);
+    }
+  });
+}
+
+function openTask(index) {
+  renderToDoModalCard(index);
+  renderSubtasksOverlay(index);
+  let parent = document.getElementById('openTaskModal');
+  let child = document.getElementById('openTaskInModal');
+  document.getElementById('editingShowTaskIndex').value = index;
+
+  parent.classList.toggle('modal-bg-animation');
+  child.classList.toggle('modal-animation');
+}
+
+function renderToDoModalCard(index) {
+  let toDoCardModal = document.getElementById('openTaskInModal');
+  toDoCardModal.innerHTML = '';
+  const task = tasks[index];
+  toDoCardModal.innerHTML += generateToDoModalHTML(task, index);
+}
+
+function capitalizeFirstLetter(string) {
+  if (!string) return '';
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function renderSubtasksOverlay(taskIndex) {
+  const container = document.getElementById('subtask-box-overlay');
+  const subtaskBoxWrapper = document.getElementById('subtaskBoxWrapper');
+  if (!tasks || !tasks[taskIndex] || !tasks[taskIndex].subtasks) {
+    // Überprüfen, ob tasks und subtasks vorhanden sind
+    console.error('Tasks or subtasks are not available.');
+    return;
+  }
+  const subtasks = tasks[taskIndex].subtasks;
+  if (subtasks.length > 0) {
+    console.log('Found subtasks:', subtasks);
+    subtaskBoxWrapper.classList.remove('d-none');
+    container.innerHTML = ''; // Container leeren, um alte Subtasks zu entfernen
+    subtasks.forEach((subtask, index) => {
+      if (subtask.status === 'open') {
+        container.innerHTML += getSubtaskOpenHTML(taskIndex, subtask, 'updateSubtaskInOverlay', index);
+      } else {
+        container.innerHTML += getSubtaskClosedHTML(taskIndex, subtask, 'updateSubtaskInOverlay', index);
+      }
+    });
+  } else {
+    console.log('No subtasks found.');
+    subtaskBoxWrapper.classList.add('d-none');
+  }
+}
+
+function renderSubtasksInOverlay(taskIndex, container) {
+  const task = tasks[taskIndex];
+  const subtasks = task.subtasks;
+
+  if (subtasks.length > 0) {
+    subtaskBoxWrapper.classList.remove('d-none'); // Stellen Sie sicher, dass subtaskBoxWrapper definiert ist
+    subtasks.forEach((subtask, index) => {
+      if (subtask.status == 'open') {
+        container.innerHTML += getSubtaskOpenHTML(taskIndex, subtask, 'updateSubtaskInOverlay', index);
+      } else {
+        container.innerHTML += getSubtaskClosedHTML(taskIndex, subtask, 'updateSubtaskInOverlay', index);
+      }
+    });
+  }
+}
+
+function getSubtaskOpenHTML(taskIndex, subtask, functionName, index) {
+  return /*html*/ `
+    <label onchange="${functionName}(${taskIndex}, ${index})"><input id="overlayCheckbox${index}" type="checkbox"> ${subtask.name}</label>
+  `;
+}
+
+function getSubtaskClosedHTML(taskIndex, subtask, functionName, index) {
+  return /*html*/ `
+    <label onchange="${functionName}(${taskIndex}, ${index})"><input id="overlayCheckbox${index}" checked  type="checkbox"> ${subtask.name}</label>
+  `;
+}
+
+async function updateSubtaskInOverlay(taskIndex, subtaskIndex) {
+  const checkbox = document.getElementById('overlayCheckbox' + subtaskIndex);
+  const task = tasks[taskIndex];
+
+  if (checkbox.checked) {
+    task.subtasks[subtaskIndex].status = 'closed';
+  } else {
+    task.subtasks[subtaskIndex].status = 'open';
+  }
+
+  await uploadTasks();
+  renderToDoCard();
+}
+
+async function deleteTask(index) {
+  tasks.splice(index, 1);
+  try {
+    await setItem('tasks', JSON.stringify(tasks));
+    console.log('Task erfolgreich gelöscht!');
+  } catch (error) {
+    console.error('Fehler beim Löschen des Tasks:', error);
+  }
+
+  let parent = document.getElementById('openTaskModal');
+  let child = document.getElementById('openTaskInModal');
+  parent.classList.remove('modal-bg-animation');
+  child.classList.remove('modal-animation');
+  renderToDoCard();
+  showUserFeedbackMessage('Task successfully deleted');
+}
+
+// Hauptfunktion zum Speichern des bearbeiteten Tasks
+function saveEditedTask(index) {
+  updateTaskDetails(index);
+  setItem('tasks', JSON.stringify(tasks))
+    .then(() => {
+      renderToDoCard();
+      openTask(document.getElementById('editingShowTaskIndex').value);
+
+      let parent = document.getElementById('editTaskModal'); // Das Modal schließen
+      let child = document.getElementById('editTaskInModal');
+      parent.classList.toggle('modal-bg-animation');
+      child.classList.toggle('modal-animation');
+    })
+    .catch(err => {
+      console.error('Error updating the task:', err);
+    });
+}
+
+// Hilfsfunktion zum Aktualisieren der Taskdetails
+function updateTaskDetails(index) {
+  const editedTitle = document.getElementById('edit-title').value;
+  const editedDescription = document.getElementById('edit-description').value;
+  const editedDueDate = document.getElementById('edit-due-date').value;
+  const EditedPriority = editedPriority;
+  const editedContacts = getSelectedContacts();
+
+  tasks[index].title = editedTitle;
+  tasks[index].description = editedDescription;
+  tasks[index].dueDate = editedDueDate;
+  tasks[index].priority = EditedPriority;
+  tasks[index].contacts = editedContacts;
+  tasks[index].subtasks = currentSubtasks;
+}
+
+// Hauptfunktion zum Setzen der bearbeiteten Priorität
+function setEditPrio(prioType) {
+  const boxes = ['edit-urgent-prio-box', 'edit-medium-prio-box', 'edit-low-prio-box'];
+
+  // Alle Priority-Boxen zurücksetzen
+  resetPriorityBoxes(boxes);
+
+  // Aktivieren der ausgewählten Priority-Box
+  const currentBox = document.getElementById(`edit-${prioType}-prio-box`);
+  currentBox.classList.add(prioType);
+  const whiteIcon = currentBox.querySelector('.white-icon');
+  whiteIcon.classList.remove('d-none');
+  const coloredIcon = currentBox.querySelector(':nth-child(2)');
+  coloredIcon.classList.add('d-none');
+
+  // Speichern der bearbeiteten Priorität in einer neuen Variable
+  editedPriority = prioType;
+}
+
+// Hilfsfunktion zum Zurücksetzen der Priority-Boxen
+function resetPriorityBoxes(boxes) {
+  boxes.forEach(boxId => {
+    const box = document.getElementById(boxId);
+    box.classList.remove('urgent', 'medium', 'low');
+    const whiteIcon = box.querySelector('.white-icon');
+    whiteIcon.classList.add('d-none');
+    const coloredIcon = box.querySelector(':nth-child(2)');
+    coloredIcon.classList.remove('d-none');
+  });
+}
+
+function toggleDropdownEditContacts() {
+  let contactsEditList = document.getElementById('dropdownContentEditContacts');
+  if (contactsEditList.style.display === 'none' || contactsEditList.style.display === '') {
+    contactsEditList.style.display = 'block';
+  } else {
+    contactsEditList.style.display = 'none';
+  }
+}
+
+function editTaskModal(index) {
+  let task = tasks[index];
+  if (!task) {
+    console.error('task not found at index', index);
+    return;
+  }
+  currentSubtasks = [...task.subtasks];
+  let parent = document.getElementById('editTaskModal');
+  parent.innerHTML = editTaskModalHTML(task, index);
+  renderAssignedContacts('dropdownContentEditContacts', task);
+  renderSubtasks('subtask-edit-box', tasks[index]);
+  let child = document.getElementById('editTaskInModal');
+
+  document.getElementById('editingTaskIndex').value = index;
+
+  parent.classList.toggle('modal-bg-animation');
+  child.classList.toggle('modal-animation');
+  setEditPrio(task.priority);
+  openTask(document.getElementById('editingShowTaskIndex').value);
+}
+
+//////////////////////////////SEARCH
+
+let foundTasks = [];
+
+function searchTask() {
+  const input = document.getElementById('search-task-input').value.toLowerCase();
+  foundTasks = tasks.filter(task => task.title.toLowerCase().includes(input) || task.description.toLowerCase().includes(input));
+  renderFilteredTasks();
+}
+
+function renderFilteredTasks() {
+  const statusArr = ['toDo', 'inProgress', 'awaitingFeedback', 'done'];
+  statusArr.forEach(status => {
+    const card = document.getElementById(`${status}-area`);
+    if (card) {
+      card.innerHTML = ''; // Lösche die aktuellen Karten
+      foundTasks.forEach((task, index) => {
+        if (task.status === status) {
+          card.innerHTML += generateToDoHTML(task, index); // Füge die gefilterten Karten hinzu
+        }
+      });
+    } else {
+      console.error(`Element mit der ID '${status}-area' wurde nicht gefunden.`);
+    }
+  });
+}
